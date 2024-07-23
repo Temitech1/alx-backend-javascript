8d21988418c0a31089aba36083e111b730766a38
Temitech1 <temidayoalabi159@gmail.com>
2024-07-23 03:03:23 +0200

    rest parameter

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,3 +1,6 @@
-export default function returnHowManyArguments() {
+/* eslint-disable no-unused-vars */
+/* eslint-disable */
 
-}
+export default function returnHowManyArguments(...args) {
+	return args.length;
+};
