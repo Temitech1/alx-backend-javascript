d4e0af6305371ade765992ab59f267a6476702be
Temitech1 <temidayoalabi159@gmail.com>
2024-07-23 02:25:10 +0200

    block scoped updated

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,9 +1,12 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable */
+
 export default function taskBlock(trueOrFalse) {
   var task = false;
   var task2 = true;
 
   if (trueOrFalse) {
-    let task = true;
+    const task = true;
     const task2 = false;
   }
 
