97dd34713ed0f280e85f47452b2e382c1eafc69c
Temitech1 <temidayoalabi159@gmail.com>
2024-07-25 03:03:10 +0200

    8 try again

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,3 +1,6 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable */
+
 export default function divideFunction(numerator, denominator) {
     if (denominator === 0) {
       throw Error('cannot divide by 0');
