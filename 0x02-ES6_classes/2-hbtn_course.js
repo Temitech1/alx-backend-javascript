d6f3c661ffac2ba46fa836f724d9a3c0c3ed5999
Temitech1 <temidayoalabi159@gmail.com>
2024-07-29 14:07:09 +0200

    hbtn course

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,45 @@
+/* eslint-disable */
+
+export default class HolbertonCourse {
+	contstructor(name, length, students) {
+		this.name = name;
+		this.length = length;
+		this.students;
+	}
+
+	get name() {
+		return this._name;
+	}
+
+	set name(value) {
+		if (typeof value !== 'string') {
+			throw new TypeError('Name must be a string');
+		}
+		this._name = value;
+	}
+
+
+	get length() {
+		return this._length;
+	}
+
+	set length(value) {
+		if (typeof value !== 'number') {
+			throw new TypeError('lenth must be a number');
+		}
+	}
+
+	get students() {
+		return this._students;
+	}
+
+	set students(value) {
+		if (!(value instanceof Array)) {
+			throw new TypeError('Students must be an array of strings');
+		}
+		if (!value.every((student) => typeof student === 'string')) {
+			throw new TypeError('Students must be an array of strings');
+		}
+		this._students = value;
+	}
+}
